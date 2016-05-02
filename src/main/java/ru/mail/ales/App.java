b7a4569dae361948dc491927f9060454cae3e666
Ales2003 <ales_2003@mail.ru>
2016-05-02 17:55:32 +0300

    Update App.java
    commit 0.7

--- src/main/java/ru/mail/ales/App.java
+++ src/main/java/ru/mail/ales/App.java
@@ -11,19 +11,46 @@ import java.util.Properties;
  * Hello world!
  *
  */
-public class App 
+public class App extends Application
 {
-    public static void main( String[] args )
+    private final ObservableList<Product> products= FXCollections.observableArrayList();
+private final ObservableList<OrderOfProducts> orderOfProducts= FXCollections.observableArrayList();
+ private final ObservableList<ProductsInOrder> productsInOrder= FXCollections.observableArrayList();
+	
+	    public static void main( String[] args )
+	    {
+       launch(args);
+	    }
+        public void start(Stage stage) throws Exception {
+	        Scene scene = new Scene(new Group());
+             stage.setTitle("Table View Sample");
+	        stage.setWidth(500);
+	        stage.setHeight(500);
+	
+	        final Label label = new Label("OdersOProduct");
+	        label.setFont(new Font("Arial", 20));
+	
+	        ((Group) scene.getRoot()).getChildren().addAll(initLayout(),initLayout());
+	
+	        stage.setScene(scene);
+	        stage.show();
+	    }
+	
+	    private Pane initLayout(){
+	        Views views = new Window();
+	        return views.buildPane(products);
+	    }
+
+
+   /** public static void main( String[] args )
     {
         Session session = HibernateUtil.getSessionFactory().openSession();
         Product product = (Product) session.get(Product.class, 1);
         session.close();
         System.out.println(product);
+}
+    */
+    
 
 
-
-
-
-
-    }
 }
